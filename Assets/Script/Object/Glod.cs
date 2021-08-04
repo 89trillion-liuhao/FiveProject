80590fe7ff298408f76840b2d1838e2ab44fa0b9
liuhao <liuhao@89trillion.com>
2021-08-04 11:25:05 +0800

    Create Project

---
+++ Assets/Script/Object/Glod.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * 金币实体类
+ */
+public class Glod
+{
+    
+    
+    private int glodValue;
+    private bool isReally;
+
+    public int GlodValue
+    {
+            get
+            {
+                return glodValue;
+            }
+            set
+            {
+                glodValue = value;
+            }
+    }
+
+    public bool IsReally
+    {
+        get
+        {
+            return isReally;
+        }
+        set
+        {
+            isReally = value;
+        }
+    }
+}
