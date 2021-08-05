3758d89aab5023572896b4b0dfd59006fd7aabd2
liuhao <liuhao@89trillion.com>
2021-08-05 15:44:19 +0800

    finish commit

--- Assets/Script/Object/Glod.cs
+++ Assets/Script/Object/Glod.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 /**
  * 金币实体类
  */
-public class Glod
+public class Glod: MonoBehaviour
 {
     
-    
+    //金币数量
     private int glodValue;
-    private bool isReally;
+    //是否被领取
+    private bool isReally ;
+    //分数
+    private int markValue;
 
+    //析构函数
+    public Glod()
+    {
+        glodValue = 200;
+        isReally = true;
+    }
+
+    public Glod(int markValue,bool isReally)
+    {
+        glodValue = 200;
+        this.markValue = markValue;
+        this.isReally = isReally;
+    }
     public int GlodValue
     {
             get
@@ -23,6 +39,17 @@ public class Glod
             }
     }
 
+    public int MarkValue
+    {
+        get
+        {
+            return markValue;
+        }
+        set
+        {
+            markValue = value;
+        }
+    }
     public bool IsReally
     {
         get
